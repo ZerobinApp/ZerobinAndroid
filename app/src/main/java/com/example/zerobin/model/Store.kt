e6952e39de8ba15e6eecdb52a16d570c7ea42a39
Andonghyun <miehdan@gmail.com>
2021-02-15 00:36:11 +0900

    상점 상세페이지 신규 개발
    - 상점리스트에서 상점 클릭시 상점 상세페이지 이동
    - 상점 데이터를 Parcelable로 넘겨주기 위하여 "kotlin-parcelize" 추가

--- app/src/main/java/com/example/zerobin/model/Store.kt
+++ app/src/main/java/com/example/zerobin/model/Store.kt
@@ -1,9 +1,13 @@
 package com.example.zerobin.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Store(
     val name: String,
     val hashTag: String,
     val favorite: Boolean,
     val address: String,
     val imageUrl: String,
-)
+) : Parcelable
