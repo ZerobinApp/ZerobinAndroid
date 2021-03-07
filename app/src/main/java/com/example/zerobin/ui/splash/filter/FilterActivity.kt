b436ac8f4f5b7207cd263711b652de27a1aacba7
Andonghyun <miehdan@gmail.com>
2021-03-07 14:01:30 +0900

    스플래시, 필터 화면 디자인 적용
    - 정확한 디자인은 아니지만 색깔, 문구 적용

--- app/src/main/java/com/example/zerobin/ui/splash/filter/FilterActivity.kt
+++ app/src/main/java/com/example/zerobin/ui/splash/filter/FilterActivity.kt
@@ -2,24 +2,39 @@ package com.example.zerobin.ui.splash.filter
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.zerobin.ui.MainActivity
 import com.example.zerobin.R
 import com.example.zerobin.databinding.ActivityFilterBinding
+import com.example.zerobin.ui.home.HomeViewModel
 
 class FilterActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityFilterBinding
+    private lateinit var binding: ActivityFilterBinding
+    private val filterViewModel: FilterViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_filter)
+        binding.vm = filterViewModel
         binding.lifecycleOwner = this
-        binding.filterSelectNextBtn.setOnClickListener {
+
+        requestFilterList()
+        startMainActivity()
+    }
+
+    private fun requestFilterList() {
+        filterViewModel.requestFilterList()
+    }
+
+    private fun startMainActivity() {
+        binding.btnNext.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }
-
     }
 }
--- app/src/main/java/com/example/zerobin/ui/splash/filter/FilterViewModel.kt
+++ app/src/main/java/com/example/zerobin/ui/splash/filter/FilterViewModel.kt
@@ -3,6 +3,7 @@ package com.example.zerobin.ui.splash.filter
 import androidx.lifecycle.ViewModel
 
 class FilterViewModel : ViewModel() {
+    fun requestFilterList() {
 
-
+    }
 }
