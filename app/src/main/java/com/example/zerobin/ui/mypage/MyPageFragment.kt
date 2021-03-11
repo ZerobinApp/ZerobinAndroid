1974b22792e846f55fd7a005e7c12ee0ddaf3c48
안동현 <andonghyun@mobmail.shinhan.com>
2021-03-11 21:58:35 +0900

    Base Activity, Fragment, ViewModel 생성
    - Base 상속받도록 구현

--- app/src/main/java/com/example/zerobin/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/example/zerobin/ui/mypage/MyPageFragment.kt
@@ -1,56 +1,62 @@
 package com.example.zerobin.ui.mypage
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.example.zerobin.R
 import com.example.zerobin.databinding.FragmentMyPageBinding
+import com.example.zerobin.ui.common.BaseBindingFragment
 
-class MyPageFragment : Fragment() {
+class MyPageFragment : BaseBindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
 
-    private lateinit var myPageViewModel: MyPageViewModel
-    private lateinit var binding: FragmentMyPageBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        myPageViewModel =
-            ViewModelProvider(this).get(MyPageViewModel::class.java)
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_page, container, false)
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.userVM = myPageViewModel
-
-        binding.nextBtnFavoriteReview.setOnClickListener {
-            setFragment(MyPageReviewFragment())
-        }
-        binding.nextBtnFavoriteShop.setOnClickListener {
-            setFragment(MyPageShopFragment())
-        }
-        binding.nextBtnFavoriteStamp.setOnClickListener {
-            setFragment(MyPageStampFragment())
-        }
-
-
-        return binding.root
-    }
+    private val myPageViewModel: MyPageViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.userVM = myPageViewModel
 
         requestReviewList()
-
+        setListeners()
+        observeLiveData()
     }
 
     private fun requestReviewList() {
         myPageViewModel.requestMyPage()
     }
 
+    private fun setListeners() {
+        binding.nextBtnFavoriteReview.setOnClickListener {
+            setFragment(MyPageReviewFragment())
+        }
+
+        binding.nextBtnFavoriteShop.setOnClickListener {
+            setFragment(MyPageShopFragment())
+        }
+
+        binding.nextBtnFavoriteStamp.setOnClickListener {
+            setFragment(MyPageStampFragment())
+        }
+    }
+
+    private fun observeLiveData() {
+        myPageViewModel.isLoading.observe(viewLifecycleOwner) { event ->
+            event.getContentIfNotHandled()?.let {
+                if (it) {
+                    showLoading()
+                } else {
+                    hideLoading()
+                }
+            }
+        }
+
+        myPageViewModel.isError.observe(viewLifecycleOwner) { event ->
+            event.getContentIfNotHandled()?.let { message ->
+                Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 
     private fun setFragment(fragment: Fragment) {
         val transaction = parentFragmentManager.beginTransaction()
